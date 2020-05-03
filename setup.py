91c5f2876cd8c73bb3dec0eefa02ece29dc26c4e
Ty Andrews <ty.elgin.andrews@gmail.com>
2020-05-03 22:32:04 +1200

    Setup Project Folder Structure
    Used from cookecutter-data-science: https://drivendata.github.io/cookiecutter-data-science/

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A GAN for generating novel pottery designs.',
+    author='Ty Andrews, Dustin Andrews',
+    license='MIT',
+)
